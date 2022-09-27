6151fae60f76fffc7263a8169fecb06eb256528b
Md Antonin Islam <antonin686.dev@gmail.com>
2022-09-27 12:36:01 +0600

    Updates Counter tests

--- test/Counter.ts
+++ test/Counter.ts
@@ -2,46 +2,55 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Counter contract", function () {
+describe("Counter", () => {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
-  async function deployCounterFixture() {
+  const deployCounterFixture = async () => {
     const Counter = await ethers.getContractFactory("Counter");
     const counter = await Counter.deploy("Antonin", 5);
 
     await counter.deployed();
 
     return { counter };
-  }
+  };
 
-  describe("Deployment", function () {
-    it("Should set the right initial name", async function () {
+  describe("Deployment", () => {
+    it("Should set the initial name", async () => {
       const { counter } = await loadFixture(deployCounterFixture);
 
       expect(await counter.getName()).to.equal("Antonin");
     });
 
-    it("Should set the right initial count", async function () {
+    it("Should set the initial count", async () => {
       const { counter } = await loadFixture(deployCounterFixture);
 
       expect(await counter.getCount()).to.equal(5);
     });
   });
 
-  describe("Counting", function () {
-    it("Increment", async function () {
+  describe("Counting", () => {
+    it("Should increment the count", async () => {
       const { counter } = await loadFixture(deployCounterFixture);
       await counter.increment();
 
       expect(await counter.getCount()).to.equal(6);
     });
 
-    it("Decrement", async function () {
+    it("Should decrement the count", async () => {
       const { counter } = await loadFixture(deployCounterFixture);
       await counter.decrement();
 
       expect(await counter.getCount()).to.equal(4);
+    });
+  });
+
+  describe("Naming", () => {
+    it("Should set the new name", async () => {
+      const { counter } = await loadFixture(deployCounterFixture);
+      await counter.setName("Abid");
+
+      expect(await counter.getName()).to.equal("Abid");
     });
   });
 });
